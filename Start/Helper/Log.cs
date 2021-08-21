926405da287739572200ac46c96ecd41e8cbbd1a
Administrator <Administrator@DESKTOP-V6G6EJE>
2021-08-21 17:30:04 +0800

    添加Excel日志记录

--- Start/Helper/Log.cs
+++ Start/Helper/Log.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
+using System.Text;
+using System.Threading;
+
+using HalconDotNet;
+
+using HYProject.Helper;
+
+using NPOI.SS.Formula.Functions;
 
 using ToolKit.HYControls.HYForm;
 
@@ -31,6 +41,8 @@ namespace HYProject
             {
                 m_lstLog["warn_logo"].Warn(strWarnLog);
                 HYForm_Log.Instance.OutputMsg(strWarnLog, System.Drawing.Color.Orange);
+                WriteLogCSV(strWarnLog, "警告");
+
             }
         }
 
@@ -49,6 +61,7 @@ namespace HYProject
 
                 m_lstLog["error_logo"].Error("<类名:" + methodBase.ReflectedType.Name + ">   <方法名:" + methodBase.Name + ">   <信息:" + strErrLog + ">", ex);
                 HYForm_Log.Instance.OutputMsg(strErrLog, System.Drawing.Color.Red);
+                WriteLogCSV(strErrLog, "异常");
             }
         }
 
@@ -63,7 +76,82 @@ namespace HYProject
             {
                 m_lstLog["run_logo"].Info(runmessage);
                 HYForm_Log.Instance.OutputMsg(runmessage, System.Drawing.Color.Green);
+                WriteLogCSV(runmessage, "正常");
             }
         }
+
+        /// <summary>
+        /// 队列
+        /// </summary>
+        private static ConcurrentQueue<LogInfo> queues = new ConcurrentQueue<LogInfo>();
+        static Thread SaveSCV;
+
+        private static void WriteLogCSV(string MESSAGE, string TYPE)
+        {
+            queues.Enqueue(new LogInfo() { datetime = DateTime.Now, type = TYPE, message = MESSAGE });
+            if (SaveSCV == null)
+            {
+                SaveSCV = new Thread(AutoSaveCSV);
+                SaveSCV.Start();
+            }
+        }
+        private static object obj = new object();
+
+        private static void AutoSaveCSV()
+        {
+            while (true)
+            {
+                try
+                {
+                    lock (obj)
+                    {
+                        LogInfo info = null;
+                        var isExit = queues.TryDequeue(out info);
+                        if (!isExit)
+                        {
+                            Thread.Sleep(500);
+                            continue;
+                        }
+                        try
+                        {
+                            if (!System.IO.Directory.Exists(@"Logs\\ALL\\"))
+                            {
+                                System.IO.Directory.CreateDirectory(@"Logs\\ALL\\");//不存在就创建目录 
+                            }
+                            if (File.Exists(@"Logs\\ALL\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".csv"))
+                            {
+                                //存在
+                                File.AppendAllText(@"Logs\\ALL\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".csv", info.datetime.ToString("yyyy-MM-dd HH:mm:ss:ffff") + "," + info.type + "," + info.message + "\n", Encoding.Default);
+                            }
+                            else
+                            {
+                                File.AppendAllText(@"Logs\\ALL\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".csv", "时间,类型,信息\n", Encoding.Default);
+                                File.AppendAllText(@"Logs\\ALL\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".csv", info.datetime.ToString("yyyy-MM-dd HH:mm:ss:ffff") + "," + info.type + "," + info.message + "\n", Encoding.Default);
+                            }
+                        }
+                        catch
+                        {
+                        }
+                    }
+
+
+
+                }
+                catch
+                {
+                }
+
+            }
+        }
+    }
+
+
+    class LogInfo
+    {
+        public string type;
+        public DateTime datetime;
+        public string message;
+
+
     }
 }
